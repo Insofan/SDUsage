b7dc26e6719bd5e28a02698ceca23b434bb36d77
Insofan <282132315@qq.com>
2019-04-04 16:41:17 +0800

    init

---
+++ SDUsage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDUsage
+//
+//  Created by Insomnia on 2019/4/4.
+//  Copyright © 2019 Insomnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDUsage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDUsage
+//
+//  Created by Insomnia on 2019/4/4.
+//  Copyright © 2019 Insomnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
